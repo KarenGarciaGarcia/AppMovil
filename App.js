0c18f83a660fbbeb0be3813cad249dc923416b44
Karenn <karenggarcia00@gmail.com>
2025-09-24 23:43:51 -0600

    Act13-Configurar en el navegador principal(App.js) usando React Navigation

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import HomeAlumnos from "./src/Pages/Pantalla_Inicio/HomeAlumnos";
 import HomePrincipal from "./src/Pages/Pantalla_Inicio/HomePrincipal";
+
+// Crear el stack
+const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   return (
-    // <HomePrincipal/>,
-    <HomeAlumnos/>
+   <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomePrincipal">
+        <Stack.Screen name="HomePrincipal" component={HomePrincipal} options={{ title: 'Inicio' }} />
+        <Stack.Screen name="HomeAlumnos" component={HomeAlumnos} options={{ title: 'Alumnos' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
     
   );
