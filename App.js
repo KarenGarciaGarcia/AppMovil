870287050af6e907ef0734a7cea2dbe0dfc3d28b
Karenn <karenggarcia00@gmail.com>
2025-09-24 23:04:20 -0600

    Act8-Crear una segunda pantalla para navegar (Para la Gestión de Alumnos)

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import HomeAlumnos from "./src/Pages/Pantalla_Inicio/HomeAlumnos";
 import HomePrincipal from "./src/Pages/Pantalla_Inicio/HomePrincipal";
 
 
 export default function App() {
   return (
-    <HomePrincipal/>
+    // <HomePrincipal/>,
+    <HomeAlumnos/>
+
     
   );
 }
